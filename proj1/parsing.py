272cc101339aed00f991b9ac9b1fb0fd3160433b
Kevin Graney <nanonet@gmail.com>
2015-02-22 11:13:59 -0500

    Improve text tokenizing logic
    Changed text tokenizing to use NLTK's punkt algorithm.  Added the
    removal of stopwords and tokens that do not contain at least one word
    character (i.e. numbers).

--- proj1/parsing.py
+++ proj1/parsing.py
@@ -1,5 +1,12 @@
 """Method for parsing HTML pages and tokenizing text."""
+import os
+import re
+
 import bs4
+import nltk
+
+nltk.data.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                      'nltk_data'))
 
 def extract_page_text(html):
     """Extracts the useful text from an HTML page.
@@ -24,6 +31,23 @@ def extract_page_text(html):
 
     return text
 
+
+_STOP_WORDS = nltk.corpus.stopwords.words('english')
+
+def filter_tokens(x):
+    """Filter to remove undesirable tokens from the list.
+
+    Args:
+      x: a string of the token
+    Returns:
+      True if the token is desirable and false if it is undesirable.
+    """
+    if x in _STOP_WORDS:
+        return False
+    if not re.search(r'\w', x):
+        # Does not contain at least one word character
+        return False
+    return True
+
 def tokenize_text(text):
-    # TODO(kevin): consider more intelligent term lexing here
-    return [x.lower() for x in text.split()]
+    return [x for x in nltk.word_tokenize(text.lower()) if filter_tokens(x)]
