52b873bd7f83ddb80e69b098c3bb3de7685dc0ae
Kevin Graney <nanonet@gmail.com>
2015-02-24 22:49:14 -0500

    Add a method to make a vector into a unit vector

--- proj1/vector_model.py
+++ proj1/vector_model.py
@@ -1,4 +1,5 @@
 import parsing
+import math
 
 class Vector(object):
     """Represents a vector of weighted terms in the Vector model.
@@ -27,15 +28,32 @@ class Vector(object):
         """Multiplication of a vector by a scalar or another vector.
 
         Args:
-          other: a scalar value to scale the vector weights by
+          other: a scalar value to scale the vector weights by or a vector to
+              take the dot product with
+        Returns:
+          a new Vector in the case of scalar multiplication or a scalar in the
+          case of multiplication with another vector
         """
-        new_terms = {}
         if type(self) == type(other):  # Vector * vector
+            dotproduct_sum = 0
             for term, weight in self.term_weights.items():
-                new_terms[term] = weight * other.get_weight(term)
+                dotproduct_sum += weight * other.get_weight(term)
+            return dotproduct_sum
         else:  # Scalar * vector
+            new_terms = {}
             for term in self.term_weights:
                 new_terms[term] = other * self.term_weights[term]
+            return type(self)(new_terms)
+
+    def __div__(self, other):
+        """Division of a vector by a scalar.
+
+        Args:
+          other: a scalar value to divide the vector weights by
+        """
+        new_terms = {}
+        for term in self.term_weights:
+            new_terms[term] = self.term_weights[term] / other
         return type(self)(new_terms)
 
     def __add__(self, other):
@@ -50,6 +68,15 @@ class Vector(object):
 
     def __str__(self):
         return str(self.term_weights)
+
+    @property
+    def magnitude(self):
+        return math.sqrt(sum(x**2 for x in self.term_weights.values()))
+
+    def make_unit(self):
+        magnitude = self.magnitude
+        for key in self.term_weights:
+            self.term_weights[key] /= magnitude
 
     @classmethod
     def build_from_text(cls, text):
--- proj1/vector_model_test.py
+++ proj1/vector_model_test.py
@@ -1,5 +1,6 @@
 import vector_model
 import unittest
+import math
 
 
 class TestVector(unittest.TestCase):
@@ -8,7 +9,7 @@ class TestVector(unittest.TestCase):
         v1 = vector_model.Vector({'a': 1, 'b': 2, 'c': 0, 'd': 0.4})
         v2 = 2*v1
         v3 = vector_model.Vector({'a': 9, 'e': 5, 'c': 0, 'd': 0.4})
-        v4 = v1*v3
+        p4 = v1*v3
         self.assertEqual(1, v1.get_weight('a'))
         self.assertEqual(2, v1.get_weight('b'))
         self.assertEqual(0, v1.get_weight('c'))
@@ -18,11 +19,7 @@ class TestVector(unittest.TestCase):
         self.assertEqual(0, v2.get_weight('c'))
         self.assertEqual(0.8, v2.get_weight('d'))
 
-        self.assertEqual(9, v4.get_weight('a'))
-        self.assertEqual(0, v4.get_weight('b'))
-        self.assertEqual(0, v4.get_weight('c'))
-        self.assertAlmostEqual(0.16, v4.get_weight('d'), places=8)
-        self.assertEqual(0, v4.get_weight('e'))
+        self.assertAlmostEqual(9.16, p4, places=8)
 
     def test_add(self):
         v1 = vector_model.Vector({'a': 1, 'b': 2, 'c': 0, 'd': 0.4})
@@ -34,6 +31,17 @@ class TestVector(unittest.TestCase):
         self.assertEqual(0.4, vs.get_weight('d'))
         self.assertEqual(0, vs.get_weight('e'))
         self.assertEqual(0.4, vs.get_weight('f'))
+
+    def test_make_unit(self):
+        v1 = vector_model.Vector({'a': 1, 'b': 2, 'c': 0, 'd': 0.4})
+        v1_orig = v1
+        self.assertNotEqual(1, v1.magnitude)
+        v1.make_unit()
+        self.assertEqual(1, v1.magnitude)
+
+        # Make sure the two vectors point in the same direction
+        self.assertEqual(0, math.acos((v1 * v1_orig) /
+                                      (v1.magnitude * v1_orig.magnitude)))
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
